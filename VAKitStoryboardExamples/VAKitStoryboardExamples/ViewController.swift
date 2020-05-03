3106803c62ef44610c3b563886c1ae3306b66fb9
VAndrJ <vandrju@gmail.com>
2020-05-03 13:49:11 +0300

    Add empty project

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VAKitStoryboardExamples
+//
+//  Created by VAndrJ on 02.05.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
