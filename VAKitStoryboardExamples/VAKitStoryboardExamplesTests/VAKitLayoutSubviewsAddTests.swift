1c54d1fd7a730269e3cea60c063b49dbdfc79182
VAndrJ <vandrju@gmail.com>
2020-05-05 23:16:33 +0300

    Add test file

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamplesTests/VAKitLayoutSubviewsAddTests.swift
@@ -0,0 +1,12 @@
+//
+//  VAKitLayoutSubviewsAddTests.swift
+//  VAKitStoryboardExamplesTests
+//
+//  Created by VAndrJ on 05.05.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import XCTest
+
+class VAKitLayoutSubviewsAddTests: XCTestCase {
+}
