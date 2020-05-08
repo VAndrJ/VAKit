d16ea7e7cf65c03f3ed76d96f473225704516c7c
VAndrJ <vandrju@gmail.com>
2020-05-08 08:34:59 +0300

    Add test for size height constraint set

--- VAKitStoryboardExamples/VAKitStoryboardExamplesTests/VAKitLayoutTestSizeConstraints.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamplesTests/VAKitLayoutTestSizeConstraints.swift
@@ -11,7 +11,8 @@ import XCTest
 class VAKitLayoutTestSizeConstraints: XCTestCase {
     
     func testFunction_sizeHeight_returnTypeCheck() {
-        
+        let view: UIView = MockView().size(height: 10)
+        XCTAssertTrue(view is MockView)
     }
 }
 
