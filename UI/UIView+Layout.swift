7afe2c44d06e68f295cf5dbe0fcc783242926d16
VAndrJ <vandrju@gmail.com>
2020-05-09 18:55:42 +0300

    Implement relation

--- UI/UIView+Layout.swift
+++ UI/UIView+Layout.swift
@@ -34,7 +34,7 @@ extension UIView {
     @discardableResult
     func size(height: CGFloat, relation: NSLayoutConstraint.Relation = .equal, priority: Float = 1000, configuring: (NSLayoutConstraint) -> Void) -> Self {
         assert(0...1000 ~= priority)
-        let constraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: height)
+        let constraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: relation, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: height)
         constraint.priority = UILayoutPriority(rawValue: priority)
         constraint.isActive = true
         configuring(constraint)
