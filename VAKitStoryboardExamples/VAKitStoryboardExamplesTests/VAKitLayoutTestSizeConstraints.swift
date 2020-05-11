9b2ba2182fe7e64a967e006936cb319fec459207
VAndrJ <vandrju@gmail.com>
2020-05-11 15:46:10 +0300

    Rename test

--- VAKitStoryboardExamples/VAKitStoryboardExamplesTests/VAKitLayoutTestSizeConstraints.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamplesTests/VAKitLayoutTestSizeConstraints.swift
@@ -24,7 +24,7 @@ class VAKitLayoutTestSizeConstraints: XCTestCase {
     
     // MARK: - Height constraint
     
-    func testFunction_sizeHeight_returnTypeCheck() {
+    func testFunction_sizeHeight_returnTypeCheckView() {
         view = MockView().size(height: 10)
         XCTAssertTrue(view is MockView)
     }
