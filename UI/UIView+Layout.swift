6ab8e08384b9daf6547236551e684f17f535875f
VAndrJ <vandrju@gmail.com>
2020-05-18 08:14:22 +0300

    Add device parameter

--- UI/UIView+Layout.swift
+++ UI/UIView+Layout.swift
@@ -44,11 +44,12 @@ extension UIView {
     }
     
     @discardableResult
-    func size(height: CGFloat, relation: NSLayoutConstraint.Relation = .equal, priority: Float = 1000, isActive: Bool = true, configuring: (NSLayoutConstraint) -> Void = { _ in }) -> Self {
+    func size(height: CGFloat, relation: NSLayoutConstraint.Relation = .equal, priority: Float = 1000, isActive: Bool = true, device: VADevice = .unspecified, configuring: (NSLayoutConstraint) -> Void = { _ in }) -> Self {
         assert(0...1000 ~= priority)
         let constraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: relation, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: height)
         constraint.priority = UILayoutPriority(rawValue: priority)
         constraint.isActive = isActive
+        constraint.identifier = device.rawValue
         configuring(constraint)
         return self
     }
