1a3a447bdb6d7c17ca4f20b7d796d84eff1acd93
VAndrJ <vandrju@gmail.com>
2020-05-19 20:25:34 +0300

    Add test class

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamplesTests/VAKitLayoutVADeviceTests.swift
@@ -0,0 +1,12 @@
+//
+//  VAKitLayoutVADeviceTests.swift
+//  VAKitStoryboardExamplesTests
+//
+//  Created by Vladimir Andrienko on 19.05.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import XCTest
+
+class VAKitLayoutVADeviceTests: XCTestCase {
+}
