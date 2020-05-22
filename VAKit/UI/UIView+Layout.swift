1ffbe696c25c56ae54029ce93f946bc3a2f0bec2
VAndrJ <vandrju@gmail.com>
2020-05-22 08:54:31 +0300

    assert add

--- VAKit/UI/UIView+Layout.swift
+++ VAKit/UI/UIView+Layout.swift
@@ -70,6 +70,7 @@ extension UIView {
     @discardableResult
     func size(_ size: CGFloat, relation: NSLayoutConstraint.Relation = .equal, priority: Float = 1000, isActive: Bool = true, device: VADevice = .unspecified, configuring: ((width: NSLayoutConstraint, height: NSLayoutConstraint)) -> Void = { _ in }) -> Self {
         assert(0...1000 ~= priority)
+        assert(size.sign == .plus)
         let widthConstraint = NSLayoutConstraint(item: self, attribute: .width, relatedBy: relation, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: size)
         widthConstraint.priority = UILayoutPriority(rawValue: priority)
         widthConstraint.identifier = device.rawValue
