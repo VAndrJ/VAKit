912043fc4cb50888504d42ae850f00f367b5e83b
VAndrJ <vandrju@gmail.com>
2020-05-23 13:33:17 +0300

    Add test class

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamplesTests/VAKitLayoutTestSuperConstraints.swift
@@ -0,0 +1,12 @@
+//
+//  VAKitLayoutTestSuperConstraints.swift
+//  VAKitStoryboardExamplesTests
+//
+//  Created by Vladimir Andrienko on 23.05.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import XCTest
+
+class VAKitLayoutTestSuperConstraints: XCTestCase {
+}
