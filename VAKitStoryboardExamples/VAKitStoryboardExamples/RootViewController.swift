b2cac773cb6459454623adff611ccf9eb203ec3e
VAndrJ <vandrju@gmail.com>
2020-06-06 20:22:25 +0300

    Remove unused & organize

--- VAKitStoryboardExamples/VAKitStoryboardExamples/RootViewController.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  VAKitStoryboardExamples
 //
 //  Created by VAndrJ on 02.05.2020.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
