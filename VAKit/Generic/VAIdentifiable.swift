009b5e03dcb6c2139b61778475943dac01de32b4
VAndrJ <vandrju@gmail.com>
2020-06-07 20:03:15 +0300

    Add identifiable

---
+++ VAKit/Generic/VAIdentifiable.swift
@@ -0,0 +1,9 @@
+//
+//  VAIdentifiable.swift
+//  VAKitStoryboardExamples
+//
+//  Created by Vladimir Andrienko on 07.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import Foundation
