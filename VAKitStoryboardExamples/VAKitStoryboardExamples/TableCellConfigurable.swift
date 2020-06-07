f03434bdb90371e5e3b3f54c8cc010b3be13a320
VAndrJ <vandrju@gmail.com>
2020-06-07 20:06:43 +0300

    Add Table cell configurable file

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/TableCellConfigurable.swift
@@ -0,0 +1,9 @@
+//
+//  TableCellConfigurable.swift
+//  VAKitStoryboardExamples
+//
+//  Created by Vladimir Andrienko on 07.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import Foundation
