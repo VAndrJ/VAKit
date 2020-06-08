ad26519433fd94b090833d0db268e39b0f31a88d
VAndrJ <vandrju@gmail.com>
2020-06-08 22:40:06 +0300

    Add implementation

--- VAKitStoryboardExamples/VAKitStoryboardExamples/TableSourceDelegate.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/TableSourceDelegate.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 VAndrJ. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+/**
+ Угу, нарушение ваших SRP, но удобный в использовании костыль для простейших случаев.
+ */
+class TableSourceDelegate: NSObject {
+    private(set) var tableData: [TableCellConfigurable & VAIdentifiable] = []
+    private weak var tableView: UITableView?
+    
+    init(tableView: UITableView, tableData: [TableCellConfigurable & VAIdentifiable]? = nil) {
+        self.tableView = tableView
+        if let tableData = tableData {
+            self.tableData = tableData
+        }
+    }
+    
+    func update(data: [TableCellConfigurable & VAIdentifiable]) {
+        self.tableData = data
+        tableView?.reloadData()
+    }
+}
