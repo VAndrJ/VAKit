57421291a684a2076abbd6715c63c610ddd83337
VAndrJ <vandrju@gmail.com>
2020-06-08 22:14:25 +0300

    Add test class

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamplesTests/VAKitVAIdentifiableTests.swift
@@ -0,0 +1,12 @@
+//
+//  VAKitVAIdentifiableTests.swift
+//  VAKitStoryboardExamplesTests
+//
+//  Created by Vladimir Andrienko on 08.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import XCTest
+
+class VAKitVAIdentifiableTests: XCTestCase {
+}
