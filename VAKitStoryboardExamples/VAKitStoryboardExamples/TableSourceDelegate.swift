027e3b4cff4cf1ccbb2295e560498ac0f76e8783
VAndrJ <vandrju@gmail.com>
2020-06-08 22:36:45 +0300

    Add tablesourcedelegate class

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/TableSourceDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  TableSourceDelegate.swift
+//  VAKitStoryboardExamples
+//
+//  Created by Vladimir Andrienko on 08.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import Foundation
