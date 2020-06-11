ee60f768b901dbd09aaa0e84cfb0911e4744c4d8
VAndrJ <vandrju@gmail.com>
2020-06-11 23:11:16 +0300

    Add VA controller

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/VAViewController.swift
@@ -0,0 +1,9 @@
+//
+//  VAViewController.swift
+//  VAKitStoryboardExamples
+//
+//  Created by Vladimir Andrienko on 11.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import Foundation
