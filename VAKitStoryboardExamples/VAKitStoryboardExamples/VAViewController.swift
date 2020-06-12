ff85f3aaf2eda00cb062a622b8a65a0a52b81966
VAndrJ <vandrju@gmail.com>
2020-06-12 21:18:35 +0300

    Add unique constraints arrays

--- VAKitStoryboardExamples/VAKitStoryboardExamples/VAViewController.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/VAViewController.swift
@@ -38,6 +38,9 @@ class VAViewController: UIViewController {
      */
     private var rWrHConstraints: [NSLayoutConstraint] = []
     
+    private lazy var rWcHUniqueConstraints: [NSLayoutConstraint] = Array(Set(rWcHConstraints).subtracting(cWcHConstraints))
+    private lazy var cWcHUniqueConstraints: [NSLayoutConstraint] = Array(Set(cWcHConstraints).subtracting(rWcHConstraints))
+    
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
