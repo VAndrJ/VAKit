fb9e8a1c87df01a38e60825fc8fb22186dd696d8
VAndrJ <vandrju@gmail.com>
2020-06-16 22:53:49 +0300

    Add field

--- VAKitStoryboardExamples/VAKitStoryboardExamples/VAConstraintable.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/VAConstraintable.swift
@@ -10,4 +10,11 @@ import UIKit
 
 protocol VAConstraintable {
     
+    /*
+     Compact width, regular height.
+     Devices like iPhone in portrait orientation
+     or iPad split screen in portrait.
+     */
+    var cWrHConstraints: [NSLayoutConstraint] { get set }
+    
 }
