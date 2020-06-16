287b5f767e8e833c6e0b38749ba6d4e60d962c2a
VAndrJ <vandrju@gmail.com>
2020-06-16 22:51:43 +0300

    Add file

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/VAConstraintable.swift
@@ -0,0 +1,9 @@
+//
+//  VAConstraintable.swift
+//  VAKitStoryboardExamples
+//
+//  Created by Vladimir Andrienko on 16.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import Foundation
