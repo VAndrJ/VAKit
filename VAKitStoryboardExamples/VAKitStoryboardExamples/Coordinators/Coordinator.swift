4b48c88a1c45cbd1007e3bfe66f82954d0b86bd8
VAndrJ <vandrju@gmail.com>
2020-06-19 21:01:53 +0300

    Add coordinator file

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/Coordinators/Coordinator.swift
@@ -0,0 +1,9 @@
+//
+//  Coordinator.swift
+//  VAKitStoryboardExamples
+//
+//  Created by Vladimir Andrienko on 19.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import Foundation
