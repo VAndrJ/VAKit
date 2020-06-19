b3ed4340e787564289cfceda92f303b4a76d9ee7
VAndrJ <vandrju@gmail.com>
2020-06-19 20:50:23 +0300

    Remove wrong

--- VAKitStoryboardExamples/VAKitStoryboardExamples/TableCells/RootTableCell/RootTableCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RootTableCell.swift
-//  VAKitStoryboardExamples
-//
-//  Created by Vladimir Andrienko on 09.06.2020.
-//  Copyright © 2020 VAndrJ. All rights reserved.
-//
-
-import Foundation
