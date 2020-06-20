2327de142cedae90bb130c768f434de98a7de95b
VAndrJ <vandrju@gmail.com>
2020-06-20 15:01:40 +0300

    Implement test

--- VAKitStoryboardExamples/VAKitStoryboardExamplesTests/VAKitLayoutSuperConstraintsTests.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamplesTests/VAKitLayoutSuperConstraintsTests.swift
@@ -583,6 +583,12 @@ class VAKitLayoutSuperConstraintsTests: XCTestCase {
     }
     
     func test_toSuperAxis_horizontal_constraintSafeAreaDefault() {
+        var constraintLeading: NSLayoutConstraint!
+        var constraintTrailing: NSLayoutConstraint!
+        parentView.addAutolayoutSubview(view)
+        view.toSuperAxis(.horizontal, configuring: { (constraintLeading, constraintTrailing) = $0 })
+        XCTAssertEqual(parentView, (constraintLeading.secondItem as? UIView))
+        XCTAssertEqual(parentView, (constraintTrailing.secondItem as? UIView))
     }
     
     // MARK: - Constraints to super view's axis wrapper
