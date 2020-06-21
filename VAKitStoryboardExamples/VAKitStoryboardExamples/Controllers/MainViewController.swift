3b36d3f53e4ccfb6048fa8adc1e9dff070fdc5df
VAndrJ <vandrju@gmail.com>
2020-06-21 18:36:41 +0300

    Viewmodel begin

--- VAKitStoryboardExamples/VAKitStoryboardExamples/Controllers/MainViewController.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/Controllers/MainViewController.swift
@@ -73,7 +73,15 @@ class MainViewController: UIViewController {
 class MainViewModel: MainViewModelProtocol {
     unowned let coordinator: MainCoordinatorDelegate
     
+    let tableData: [MainTableCellEntity] = [
+        MainTableCellEntity(title: "iPhone of any size with different element size in portrait and landscape", secondaryTitle: "One centered view with different sizes. VAView used."),
+    ]
+    
     init(coordinator: MainCoordinatorDelegate) {
         self.coordinator = coordinator
+    }
+    
+    func onSelectRow(at indexPath: IndexPath) {
+        let cellData = tableData[indexPath.row]
     }
 }
