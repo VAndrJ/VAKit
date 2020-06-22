189ee47ec13fed375574402e1e6f842c1da12cee
VAndrJ <vandrju@gmail.com>
2020-06-22 19:44:03 +0300

    Init

--- VAKitStoryboardExamples/VAKitStoryboardExamples/Controllers/IPhoneAnySizeDifferentPortraitLandscapeSize/IPhoneAnySizeDifferentPortraitLandscapeSizeView.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/Controllers/IPhoneAnySizeDifferentPortraitLandscapeSize/IPhoneAnySizeDifferentPortraitLandscapeSizeView.swift
@@ -13,6 +13,16 @@ class IPhoneAnySizeDifferentPortraitLandscapeSizeView: VAView {
         $0.backgroundColor = .purple
     }
     
+    convenience init() {
+        self.init(frame: UIScreen.main.bounds)
+        addElements()
+        configure()
+    }
+    
+    private func configure() {
+        backgroundColor = .white
+    }
+    
     override func addElements() {
         addAutolayoutSubview(exampleView)
         exampleView
