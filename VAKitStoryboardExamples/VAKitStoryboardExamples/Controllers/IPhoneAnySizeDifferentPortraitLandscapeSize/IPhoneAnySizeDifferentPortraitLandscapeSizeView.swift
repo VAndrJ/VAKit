5d60155f35f4bbff7f04743ca5a395fc36c3b721
VAndrJ <vandrju@gmail.com>
2020-06-22 19:42:23 +0300

    Add file

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/Controllers/IPhoneAnySizeDifferentPortraitLandscapeSize/IPhoneAnySizeDifferentPortraitLandscapeSizeView.swift
@@ -0,0 +1,9 @@
+//
+//  IPhoneAnySizeDifferentPortraitLandscapeSizeView.swift
+//  VAKitStoryboardExamples
+//
+//  Created by Vladimir Andrienko on 22.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import Foundation
