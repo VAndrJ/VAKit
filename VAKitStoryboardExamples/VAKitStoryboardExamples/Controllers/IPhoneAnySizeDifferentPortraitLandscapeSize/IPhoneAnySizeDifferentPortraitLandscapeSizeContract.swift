5d2ae61f990a5f577900795207caa1cbd26f2a66
VAndrJ <vandrju@gmail.com>
2020-06-24 08:33:01 +0300

    Add contract file

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/Controllers/IPhoneAnySizeDifferentPortraitLandscapeSize/IPhoneAnySizeDifferentPortraitLandscapeSizeContract.swift
@@ -0,0 +1,9 @@
+//
+//  IPhoneAnySizeDifferentPortraitLandscapeSizeContract.swift
+//  VAKitStoryboardExamples
+//
+//  Created by Vladimir Andrienko on 24.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import Foundation
