93e3a9a5b060f04282b8a94670dbf61d0d7b553e
VAndrJ <vandrju@gmail.com>
2020-06-25 22:40:13 +0300

    Add file

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/Entities/Internal/MainCellEntity.swift
@@ -0,0 +1,9 @@
+//
+//  MainCellEntity.swift
+//  VAKitStoryboardExamples
+//
+//  Created by Vladimir Andrienko on 25.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import Foundation
