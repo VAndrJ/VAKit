ffff2d3dde743a48f9c5da7b1b04512b67716b57
VAndrJ <vandrju@gmail.com>
2020-06-27 20:51:26 +0300

    Add iconview file

---
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/CustomUI/VAIconView.swift
@@ -0,0 +1,9 @@
+//
+//  VAIconView.swift
+//  VAKitStoryboardExamples
+//
+//  Created by Vladimir Andrienko on 27.06.2020.
+//  Copyright © 2020 VAndrJ. All rights reserved.
+//
+
+import Foundation
