55805ef35b138899fedefd85b7a21537154a3eb4
VAndrJ <vandrju@gmail.com>
2020-06-27 20:46:48 +0300

    Add configure

--- VAKitStoryboardExamples/VAKitStoryboardExamples/Controllers/DifferentPortraitLandscapeAnyPhoneSizePadSame/DifferentPortraitLandscapeAnyPhoneSizePadSameView.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/Controllers/DifferentPortraitLandscapeAnyPhoneSizePadSame/DifferentPortraitLandscapeAnyPhoneSizePadSameView.swift
@@ -13,5 +13,15 @@ class DifferentPortraitLandscapeAnyPhoneSizePadSameView: VAView {
     convenience init() {
         self.init(frame: UIScreen.main.bounds)
         addElements()
+        configure()
+    }
+    
+    private func configure() {
+        backgroundColor = .white
+    }
+    
+    override func addElements() {
+        
+        super.addElements()
     }
 }
