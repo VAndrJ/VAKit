70ddcc2861e39352010c7b4950bb01952a9f8d93
VAndrJ <vandrju@gmail.com>
2020-06-28 10:01:50 +0300

    Shadow add method

--- VAKit/UI/UIView.swift
+++ VAKit/UI/UIView.swift
@@ -10,4 +10,11 @@ import UIKit
 
 extension UIView {
     
+    func withShadow() -> Self {
+        layer.shadowRadius = 2
+        layer.shadowOffset = .init(width: 0, height: 1)
+        layer.shadowColor = UIColor.systemPurple.cgColor
+        layer.shadowOpacity = 0.4
+        return self
+    }
 }
