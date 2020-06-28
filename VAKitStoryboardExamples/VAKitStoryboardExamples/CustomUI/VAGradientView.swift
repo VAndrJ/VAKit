1bb472bed5bc4a3528c432201186098e5f245988
VAndrJ <vandrju@gmail.com>
2020-06-28 10:02:51 +0300

    Implement gradient

--- VAKitStoryboardExamples/VAKitStoryboardExamples/CustomUI/VAGradientView.swift
+++ VAKitStoryboardExamples/VAKitStoryboardExamples/CustomUI/VAGradientView.swift
@@ -12,4 +12,23 @@ import UIKit
  Not important for example
  */
 class VAGradientView: UIView {
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+    var gradientLayer: CAGradientLayer {
+        return layer as! CAGradientLayer
+    }
+    
+    convenience init() {
+        self.init(frame: .init(x: 0, y: 0, width: 10, height: 10))
+        configure()
+    }
+    
+    private func configure() {
+        let layer = gradientLayer
+        layer.startPoint = .init(x: 0, y: 0)
+        layer.endPoint = .init(x: 1, y: 1)
+        layer.locations = [0.05, 0.95]
+        layer.colors = [UIColor.systemPurple.cgColor, UIColor.systemTeal.cgColor]
+    }
 }
