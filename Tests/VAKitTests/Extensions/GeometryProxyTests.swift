b4d1fa6d9012b75ae069d5c7988206cd6c2f392f
Volodymyr Andriienko <vandrjios@gmail.com>
2025-05-15 16:08:20 +0300

    [tests] CGPoint new init usage.

--- Tests/VAKitTests/Extensions/GeometryProxyTests.swift
+++ Tests/VAKitTests/Extensions/GeometryProxyTests.swift
@@ -16,11 +16,11 @@ struct GeometryProxyTests {
     @Test(
         "GeometryProxy center depending on size",
         arguments: [
-            (CGSize(same: 100), CGPoint(x: 50, y: 50)),
+            (CGSize(same: 100), CGPoint(xy: 50)),
             (CGSize(width: 20, height: 10), CGPoint(x: 10, y: 5)),
-            (CGSize(same: 0), CGPoint(x: 0, y: 0)),
+            (CGSize(same: 0), CGPoint(xy: 0)),
             (CGSize(width: 10, height: 20), CGPoint(x: 5, y: 10)),
-            (CGSize(same: 1000), CGPoint(x: 500, y: 500)),
+            (CGSize(same: 1000), CGPoint(xy: 500)),
         ]
     )
     func center(size: CGSize, expected: CGPoint) async throws {
