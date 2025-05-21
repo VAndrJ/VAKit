537b807e162ac0b50833eb14878fa5019f269ed2
Volodymyr Andriienko <vandrjios@gmail.com>
2025-05-21 17:07:30 +0300

    [example] Hosting app.

---
+++ Example/HostingApp/HostingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostingAppApp.swift
+//  HostingApp
+//
+//  Created by VAndrJ on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct HostingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
